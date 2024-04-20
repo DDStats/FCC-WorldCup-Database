57ed47632818f5b353ffc6ed602fcfabb7b36f4a
DDStats <65207114+DDStats@users.noreply.github.com>
2024-04-20 16:39:06 -0500

    Update worldcup.sql

--- worldcup.sql
+++ worldcup.sql
@@ -51,10 +51,10 @@ CREATE TABLE public.games (
     game_id integer NOT NULL,
     year integer NOT NULL,
     round character varying(255) NOT NULL,
-    winner_id integer NOT NULL,
-    opponent_id integer NOT NULL,
     winner_goals integer NOT NULL,
-    opponent_goals integer NOT NULL
+    opponent_goals integer NOT NULL,
+    winner_id integer NOT NULL,
+    opponent_id integer NOT NULL
 );
 
 
@@ -134,38 +134,38 @@ ALTER TABLE ONLY public.teams ALTER COLUMN team_id SET DEFAULT nextval('public.t
 -- Data for Name: games; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.games VALUES (1, 2018, 'Final', 1, 2, 4, 2);
-INSERT INTO public.games VALUES (2, 2018, 'Third Place', 3, 4, 2, 0);
-INSERT INTO public.games VALUES (3, 2018, 'Semi-Final', 2, 4, 2, 1);
-INSERT INTO public.games VALUES (4, 2018, 'Semi-Final', 1, 3, 1, 0);
-INSERT INTO public.games VALUES (5, 2018, 'Quarter-Final', 2, 5, 3, 2);
-INSERT INTO public.games VALUES (6, 2018, 'Quarter-Final', 4, 6, 2, 0);
-INSERT INTO public.games VALUES (7, 2018, 'Quarter-Final', 3, 7, 2, 1);
-INSERT INTO public.games VALUES (8, 2018, 'Quarter-Final', 1, 8, 2, 0);
-INSERT INTO public.games VALUES (9, 2018, 'Eighth-Final', 4, 9, 2, 1);
-INSERT INTO public.games VALUES (10, 2018, 'Eighth-Final', 6, 10, 1, 0);
-INSERT INTO public.games VALUES (11, 2018, 'Eighth-Final', 3, 11, 3, 2);
-INSERT INTO public.games VALUES (12, 2018, 'Eighth-Final', 7, 12, 2, 0);
-INSERT INTO public.games VALUES (13, 2018, 'Eighth-Final', 2, 13, 2, 1);
-INSERT INTO public.games VALUES (14, 2018, 'Eighth-Final', 5, 14, 2, 1);
-INSERT INTO public.games VALUES (15, 2018, 'Eighth-Final', 8, 15, 2, 1);
-INSERT INTO public.games VALUES (16, 2018, 'Eighth-Final', 1, 16, 4, 3);
-INSERT INTO public.games VALUES (17, 2014, 'Final', 17, 16, 1, 0);
-INSERT INTO public.games VALUES (18, 2014, 'Third Place', 18, 7, 3, 0);
-INSERT INTO public.games VALUES (19, 2014, 'Semi-Final', 16, 18, 1, 0);
-INSERT INTO public.games VALUES (20, 2014, 'Semi-Final', 17, 7, 7, 1);
-INSERT INTO public.games VALUES (21, 2014, 'Quarter-Final', 18, 19, 1, 0);
-INSERT INTO public.games VALUES (22, 2014, 'Quarter-Final', 16, 3, 1, 0);
-INSERT INTO public.games VALUES (23, 2014, 'Quarter-Final', 7, 9, 2, 1);
-INSERT INTO public.games VALUES (24, 2014, 'Quarter-Final', 17, 1, 1, 0);
-INSERT INTO public.games VALUES (25, 2014, 'Eighth-Final', 7, 20, 2, 1);
-INSERT INTO public.games VALUES (26, 2014, 'Eighth-Final', 9, 8, 2, 0);
-INSERT INTO public.games VALUES (27, 2014, 'Eighth-Final', 1, 21, 2, 0);
-INSERT INTO public.games VALUES (28, 2014, 'Eighth-Final', 17, 22, 2, 1);
-INSERT INTO public.games VALUES (29, 2014, 'Eighth-Final', 18, 12, 2, 1);
-INSERT INTO public.games VALUES (30, 2014, 'Eighth-Final', 19, 23, 2, 1);
-INSERT INTO public.games VALUES (31, 2014, 'Eighth-Final', 16, 10, 1, 0);
-INSERT INTO public.games VALUES (32, 2014, 'Eighth-Final', 3, 24, 2, 1);
+INSERT INTO public.games VALUES (1, 2018, 'Final', 4, 2, 1, 2);
+INSERT INTO public.games VALUES (2, 2018, 'Third Place', 2, 0, 3, 4);
+INSERT INTO public.games VALUES (3, 2018, 'Semi-Final', 2, 1, 2, 4);
+INSERT INTO public.games VALUES (4, 2018, 'Semi-Final', 1, 0, 1, 3);
+INSERT INTO public.games VALUES (5, 2018, 'Quarter-Final', 3, 2, 2, 5);
+INSERT INTO public.games VALUES (6, 2018, 'Quarter-Final', 2, 0, 4, 6);
+INSERT INTO public.games VALUES (7, 2018, 'Quarter-Final', 2, 1, 3, 7);
+INSERT INTO public.games VALUES (8, 2018, 'Quarter-Final', 2, 0, 1, 8);
+INSERT INTO public.games VALUES (9, 2018, 'Eighth-Final', 2, 1, 4, 9);
+INSERT INTO public.games VALUES (10, 2018, 'Eighth-Final', 1, 0, 6, 10);
+INSERT INTO public.games VALUES (11, 2018, 'Eighth-Final', 3, 2, 3, 11);
+INSERT INTO public.games VALUES (12, 2018, 'Eighth-Final', 2, 0, 7, 12);
+INSERT INTO public.games VALUES (13, 2018, 'Eighth-Final', 2, 1, 2, 13);
+INSERT INTO public.games VALUES (14, 2018, 'Eighth-Final', 2, 1, 5, 14);
+INSERT INTO public.games VALUES (15, 2018, 'Eighth-Final', 2, 1, 8, 15);
+INSERT INTO public.games VALUES (16, 2018, 'Eighth-Final', 4, 3, 1, 16);
+INSERT INTO public.games VALUES (17, 2014, 'Final', 1, 0, 17, 16);
+INSERT INTO public.games VALUES (18, 2014, 'Third Place', 3, 0, 18, 7);
+INSERT INTO public.games VALUES (19, 2014, 'Semi-Final', 1, 0, 16, 18);
+INSERT INTO public.games VALUES (20, 2014, 'Semi-Final', 7, 1, 17, 7);
+INSERT INTO public.games VALUES (21, 2014, 'Quarter-Final', 1, 0, 18, 19);
+INSERT INTO public.games VALUES (22, 2014, 'Quarter-Final', 1, 0, 16, 3);
+INSERT INTO public.games VALUES (23, 2014, 'Quarter-Final', 2, 1, 7, 9);
+INSERT INTO public.games VALUES (24, 2014, 'Quarter-Final', 1, 0, 17, 1);
+INSERT INTO public.games VALUES (25, 2014, 'Eighth-Final', 2, 1, 7, 20);
+INSERT INTO public.games VALUES (26, 2014, 'Eighth-Final', 2, 0, 9, 8);
+INSERT INTO public.games VALUES (27, 2014, 'Eighth-Final', 2, 0, 1, 21);
+INSERT INTO public.games VALUES (28, 2014, 'Eighth-Final', 2, 1, 17, 22);
+INSERT INTO public.games VALUES (29, 2014, 'Eighth-Final', 2, 1, 18, 12);
+INSERT INTO public.games VALUES (30, 2014, 'Eighth-Final', 2, 1, 19, 23);
+INSERT INTO public.games VALUES (31, 2014, 'Eighth-Final', 1, 0, 16, 10);
+INSERT INTO public.games VALUES (32, 2014, 'Eighth-Final', 2, 1, 3, 24);
 
 
 --
@@ -237,22 +237,37 @@ ALTER TABLE ONLY public.teams
 
 
 --
--- Name: games fk_opponent; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+-- Name: games games_opponent_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
 --
 
 ALTER TABLE ONLY public.games
-    ADD CONSTRAINT fk_opponent FOREIGN KEY (opponent_id) REFERENCES public.teams(team_id);
+    ADD CONSTRAINT games_opponent_id_fkey FOREIGN KEY (opponent_id) REFERENCES public.teams(team_id);
 
 
 --
--- Name: games fk_winner; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+-- Name: games games_winner_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
 --
 
 ALTER TABLE ONLY public.games
-    ADD CONSTRAINT fk_winner FOREIGN KEY (winner_id) REFERENCES public.teams(team_id);
+    ADD CONSTRAINT games_winner_id_fkey FOREIGN KEY (winner_id) REFERENCES public.teams(team_id);
 
 
 --
 -- PostgreSQL database dump complete
 --
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
 
